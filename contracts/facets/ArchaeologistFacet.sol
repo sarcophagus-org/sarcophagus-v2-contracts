df8c19086fcaa2b8cbd608b9e6ccaff6e0ea97bb
knoll3 <kyle.noll@decentlabs.io>
2022-06-13 16:20:56 -0500

    Fix misspelling

--- contracts/facets/ArchaeologistFacet.sol
+++ contracts/facets/ArchaeologistFacet.sol
@@ -10,79 +10,79 @@ contract ArchaeologistFacet {
     AppStorage internal s;
 
     /// @notice Deposits an archaeologist's free bond to the contract.
-    /// @param archaegologistAddress The address of the archaeologist whose
+    /// @param archaeologist The address of the archaeologist whose
     /// free bond is being deposited
     /// @param amount The amount to deposit
     /// @param sarcoToken the sarcophagus ERC20 token SARC
     function depositFreeBond(
-        address archaegologistAddress,
+        address archaeologist,
         uint256 amount,
         IERC20 sarcoToken
     ) external {
         // Confirm that sender is the archaeologist
         require(
-            msg.sender == archaegologistAddress,
+            msg.sender == archaeologist,
             "sender must be the archaeologist"
         );
 
         // Increase the archaeolgist's free bond in app storage
-        LibBonds.increaseFreeBond(archaegologistAddress, amount);
+        LibBonds.increaseFreeBond(archaeologist, amount);
 
         // Transfer the amount of sarcoToken from the archaeologist to the contract
         sarcoToken.transferFrom(msg.sender, address(this), amount);
 
         // Emit an event
-        emit LibEvents.DepositFreeBond(archaegologistAddress, amount);
+        emit LibEvents.DepositFreeBond(archaeologist, amount);
     }
 
     /// @notice Withdraws an archaeologist's free bond from the contract.
-    /// @param archaegologistAddress The address of the archaeologist whose
+    /// @param archaeologist The address of the archaeologist whose
     /// free bond is being withdrawn
     /// @param amount The amount to withdraw
     /// @param sarcoToken the sarcophagus ERC20 token SARC
     function withdrawFreeBond(
-        address archaegologistAddress,
+        address archaeologist,
         uint256 amount,
         IERC20 sarcoToken
     ) external {
         // Confirm that sender is the archaeologist
         require(
-            msg.sender == archaegologistAddress,
+            msg.sender == archaeologist,
             "sender must be the archaeologist"
         );
 
         // Decrease the archaeologist's free bond amount.
         // Reverts if there is not enough free bond on the contract.
-        LibBonds.decreaseFreeBond(archaegologistAddress, amount);
+        LibBonds.decreaseFreeBond(archaeologist, amount);
 
         // Transfer the amount of sarcoToken to the archaeologist
         sarcoToken.transfer(msg.sender, amount);
 
         // Emit an event
-        emit LibEvents.WithdrawFreeBond(archaegologistAddress, amount);
+        emit LibEvents.WithdrawFreeBond(archaeologist, amount);
     }
 
     /// @notice Returns the amount of free bond stored in the contract for an
     /// archaeologist.
-    /// @param archaegologistAddress The address of the archaeologist whose
+    /// @param archaeologist The address of the archaeologist whose
     /// free bond is being returned
-    function getFreeBond(address archaegologistAddress)
+    function getFreeBond(address archaeologist)
         external
         view
         returns (uint256)
     {
-        return s.freeBonds[archaegologistAddress];
+        return s.freeBonds[archaeologist];
     }
 
     /// @notice Returns the amount of cursed bond stored in the contract for an
     /// archaeologist.
-    /// @param archaegologistAddress The address of the archaeologist whose
+    /// @param archaeologist The address of the archaeologist whose
     /// cursed bond is being returned
-    function getCursedBond(address archaegologistAddress)
+    function getCursedBond(address archaeologist)
         external
         view
         returns (uint256)
     {
-        return s.cursedBonds[archaegologistAddress];
+        return s.cursedBonds[archaeologist];
     }
 }
