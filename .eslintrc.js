e9af7683ab5b13d347488124f69f43f9bb4d1f00
Kellar <kellar.base09@gmail.com>
2022-06-29 14:46:10 +0100

    Configure linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,51 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "prettier",
+    "node"
+  ],
   extends: [
     "standard",
     "plugin:prettier/recommended",
     "plugin:node/recommended",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: "latest",
   },
   rules: {
     "node/no-unsupported-features/es-syntax": [
       "error",
-      { ignores: ["modules"] },
+      { ignores: ["modules"] }
     ],
-    "no-unused-vars": ["warn"],
+    "comma-dangle": "off",
+    indent: "off",
+    "@typescript-eslint/indent": "off",
+    // prettier config
+    "prettier/prettier": [
+      1,
+      {
+        printWidth: 100,
+        endOfLine: "lf",
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: false,
+        semi: true,
+        arrowParens: "avoid",
+        singleAttributePerLine: true,
+      },
+    ],
+    // var rules
+    "no-use-before-define": "warn",
+    "@typescript-eslint/comma-dangle": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "@typescript-eslint/no-unused-vars": "off",
   },
   overrides: [
     {
@@ -31,6 +60,13 @@ module.exports = {
     },
   ],
   settings: {
+    // resolves imports without file extensions for listed extensions
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [".ts", ".d.ts"],
+      },
+    },
     node: {
       tryExtensions: [".js", ".json", ".ts", ".d.ts"],
     },
