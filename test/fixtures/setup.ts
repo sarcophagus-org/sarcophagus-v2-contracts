4d15cece9381d8a45ef83e16e7fe8a49450c31ad
knoll3 <kyle.noll@decentlabs.io>
2022-07-06 11:18:11 -0500

    Add fixture for core test setup

--- test/fixtures/setup.ts
+++ test/fixtures/setup.ts
@@ -1,27 +1,47 @@
-import { getContractFactory } from "@nomiclabs/hardhat-ethers/types";
+import { BigNumber } from "ethers";
 import { deployments } from "hardhat";
+import { getDeployedContracts } from "./get-deployed-contracts";
+import { setupArchaeologists } from "./setup-archaeologists";
 
-export const setup = deployments.createFixture(
+export const coreSetup = deployments.createFixture(
   async ({ deployments, getNamedAccounts, getUnnamedAccounts, ethers }) => {
     await deployments.fixture();
-    const { deployer } = await getNamedAccounts();
-    // set the first account from unnamed accounts as the embalmer
     const unnamedAccounts = await getUnnamedAccounts();
     const embalmer = await ethers.getSigner(unnamedAccounts[0]);
-    const archaeologists = [
-      await ethers.getSigner(unnamedAccounts[1]),
-      await ethers.getSigner(unnamedAccounts[2]),
-      await ethers.getSigner(unnamedAccounts[3]),
-    ];
+    const recipient = await ethers.getSigner(unnamedAccounts[1]);
 
-    const sarcoToken = await ethers.getContract("SarcoTokenMock");
-    const diamond = await ethers.getContract("Diamond_DiamondProxy");
+    const { diamond, sarcoToken, archaeologistFacet } =
+      await getDeployedContracts();
 
     // Set up the archaeologists
+    const archaeologists = await setupArchaeologists();
 
     // Approve the embalmer on the sarco token so transferFrom will work
     await sarcoToken
       .connect(embalmer)
-      .approve(diamondAddress, ethers.constants.MaxUint256);
+      .approve(diamond.address, ethers.constants.MaxUint256);
+
+    for (const archaeologist of archaeologists) {
+      // Transfer 10,000 sarco tokens to each archaeologist to be put into free
+      // bond
+      await sarcoToken.transfer(archaeologist.address, BigNumber.from(10_000));
+
+      // Approve the archaeologist on the sarco token so transferFrom will work
+      await sarcoToken
+        .connect(archaeologist.signer)
+        .approve(diamond.address, ethers.constants.MaxUint256);
+
+      // Deposit some free bond to the contract so initializeSarcophagus will
+      // work
+      await archaeologistFacet
+        .connect(archaeologist.signer)
+        .depositFreeBond(BigNumber.from("1000"));
+    }
+
+    return {
+      embalmer,
+      recipient,
+      archaeologists,
+    };
   }
 );
