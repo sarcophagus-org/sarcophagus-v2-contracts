911b611a85ed936c0b7f2434d67eea3547c51163
Kellar <kellar.base09@gmail.com>
2022-07-12 18:48:54 +0100

    bug fix

--- test/facets/archaeologist-facet.spec.ts
+++ test/facets/archaeologist-facet.spec.ts
@@ -852,7 +852,7 @@ describe("Contract: ArchaeologistFacet", () => {
           newArchaeologist.address
         );
         expect(newArchaeologistData.doubleHashedShard).to.not.equal(ethers.constants.HashZero);
-        expect(newArchaeologistData.hashedShard).to.not.equal(ethers.constants.HashZero);
+        expect(newArchaeologistData.doubleHashedShard).to.not.equal(ethers.constants.HashZero);
 
         // Check that the old archaeologist's values are reset to default values
         const oldArchaeologistData = await viewStateFacet.getSarcophagusArchaeologist(
@@ -862,7 +862,7 @@ describe("Contract: ArchaeologistFacet", () => {
 
         expect(oldArchaeologistData.doubleHashedShard).to.equal(ethers.constants.HashZero);
 
-        expect(oldArchaeologistData.hashedShard).to.equal(ethers.constants.HashZero);
+        expect(oldArchaeologistData.doubleHashedShard).to.equal(ethers.constants.HashZero);
         expect(oldArchaeologistData.diggingFee).to.equal("0");
         expect(oldArchaeologistData.bounty).to.equal("0");
       });
