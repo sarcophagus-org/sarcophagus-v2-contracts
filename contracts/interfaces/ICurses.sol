8cbbf786f07df1f6c7af50c4643c3fccc365d72c
knoll3 <kyle.noll@decentlabs.io>
2022-07-26 16:36:20 -0400

    Create svg on nft mint

--- contracts/interfaces/ICurses.sol
+++ contracts/interfaces/ICurses.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-// import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-
 interface ICurses {
     function safeTransferFrom(
         address _from,
@@ -25,8 +23,12 @@ interface ICurses {
         uint256 _sarcoId,
         string memory _name,
         string memory _description,
-        string memory _imageURI,
         string memory _diggingFee,
         string memory _bounty
     ) external;
+
+    function createSVG(string memory bounty, string memory diggingFee)
+        external
+        pure
+        returns (string memory);
 }
