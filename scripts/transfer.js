ae161cb6e78a1f2afc6e0220b773ba282f503c0b
sethhrbek <seth.hrbek@gmail.com>
2022-10-14 20:54:06 -0400

    add transfer script

---
+++ scripts/transfer.js
@@ -0,0 +1,18 @@
+// Transfer 10,000 sarco tokens to the embalmer
+const amountToTransfer =
+const hre = require("hardhat");
+
+(async () => {
+  const sarcoToken = await hre.ethers.getContractAt(
+    "SarcoTokenMock",
+    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+  );
+  const unsignedAccounts = await hre.getUnnamedAccounts();
+  await sarcoToken.transfer(
+    unsignedAccounts[0],
+    hre.ethers.BigNumber.from("10000000000000000000000")
+  );
+
+  const newBalance = await sarcoToken.balanceOf(unsignedAccounts[0])
+  console.log("embalmer balance:", newBalance.toString());
+})();
