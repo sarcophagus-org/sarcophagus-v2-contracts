baf254e10903a081e066c2a59d6dc99c3ae555f2
Breads <breads.@Breadss-MacBook-Pro.local>
2022-11-17 23:13:44 -0800

    add more test cases for accuse on a compromised sarco, remove old thirdpartyfacet tests

--- test/facets/thirdparty/accuse.spec.ts
+++ test/facets/thirdparty/accuse.spec.ts
@@ -269,7 +269,6 @@ describe("accuse v2", () => {
   });
 
   describe("Supports accusal of k or more archaeologists", function () {
-    // todo: do we refund digging fees to the embalmer for the innocent archaeologists?
     it("On a successful accusal of 3 archaeologists on a 3 of 5 sarco, should split cursed bond for the 3 leaking archs between the embalmer and the accuser, refund digging fees for 3 archaeologists to the embalmer, slash the 3 leaking archaeologists' bonds, mark the archaeologists as accused, and emit an AccuseArchaeologist event", async function () {
       const { thirdPartyFacet, viewStateFacet } = await getContracts();
       const accuser = await getFreshAccount();
@@ -390,34 +389,18 @@ describe("accuse v2", () => {
         }
       );
 
+      // verify accused archaeologists have been marked as accused
       await verifyAccusalStatusesForArchaeologists(
         sarcophagus.sarcoId,
         accusedArchaeologists,
-        false
-      );
-
-      // verify accused archaeologists have been marked as accused
-      await Promise.all(
-        accusedArchaeologists.map(async (accusedArchaeologist) => {
-          const accusedArchaeologistStorage =
-            await viewStateFacet.getSarcophagusArchaeologist(
-              sarcophagus.sarcoId,
-              accusedArchaeologist.archAddress
-            );
-          expect(accusedArchaeologistStorage.accused).to.be.true;
-        })
+        true
       );
 
       // verify innocent archaeologists have not been marked as accused
-      await Promise.all(
-        innocentArchaeologists.map(async (innocentArchaeologist) => {
-          const innocentArchaeologistStorage =
-            await viewStateFacet.getSarcophagusArchaeologist(
-              sarcophagus.sarcoId,
-              innocentArchaeologist.archAddress
-            );
-          expect(innocentArchaeologistStorage.accused).to.be.false;
-        })
+      await verifyAccusalStatusesForArchaeologists(
+        sarcophagus.sarcoId,
+        innocentArchaeologists,
+        false
       );
 
       // verify the sarcoId has been added to the accused archaeologist's archaeologistAccusals
@@ -443,7 +426,7 @@ describe("accuse v2", () => {
       );
     });
 
-    it("Should not refund bonds to other archaeologists or change sarcophagus state if less than k archaeologists have been accused", async function () {
+    it("Should allow accusal of 2 archaeologists on a 3 of 5 sarcophagus without freeing all other archaeologists", async function () {
       const accuser = await getFreshAccount();
       const { viewStateFacet } = await getContracts();
 
@@ -456,14 +439,16 @@ describe("accuse v2", () => {
             (await time.latest()) + time.duration.weeks(3),
         });
 
-      const accusedArchaeologist = archaeologists[0];
-      // hash the leaked keyshare
-      const hashedShare = hashShare(accusedArchaeologist.share);
+      const accusedArchaeologists = archaeologists.splice(0, 2);
 
       // accuse an archaeologist of leaking a keyshare
-      await (await getContracts()).thirdPartyFacet
-        .connect(accuser)
-        .accuse(sarcophagus.sarcoId, [hashedShare], accuser.address);
+      await (await getContracts()).thirdPartyFacet.connect(accuser).accuse(
+        sarcophagus.sarcoId,
+        accusedArchaeologists.map((accusedArchaeologist) =>
+          hashShare(accusedArchaeologist.share)
+        ),
+        accuser.address
+      );
 
       // verify the sarcophagus state is still active
       expect(
@@ -483,7 +468,8 @@ describe("accuse v2", () => {
         })
       );
     });
-    it("Should not pay out digging fees or cursed bond or emit an event on accusal of an archaeologist that has already been accused once", async function () {
+
+    it("Should free all unaccused archaeologists upon successful accusal of 1 archaeologist on a 3 of 5 sarcophagus where 2 have been accused on a previous call and update state to accused", async function () {
       const { thirdPartyFacet, viewStateFacet } = await getContracts();
       const accuser = await getFreshAccount();
       const { embalmer, sarcophagus, archaeologists } =
@@ -494,158 +480,222 @@ describe("accuse v2", () => {
           resurrectionTimeSeconds:
             (await time.latest()) + time.duration.weeks(3),
         });
-      const accusedArchaeologist = archaeologists[0];
+      const accusedArchaeologists = archaeologists.slice(0, 3);
+      const innocentArchaeologists = archaeologists.slice(3, 5);
+
+      // snapshot the balance of the embalmer prior to the accusal
       const embalmerPreAccuseSarquitoBalance = await getSarquitoBalance(
         embalmer.address
       );
-      // accuse the archaeologist of leaking a keyshare
-      await thirdPartyFacet
-        .connect(accuser)
-        .accuse(
-          sarcophagus.sarcoId,
-          [hashShare(accusedArchaeologist.share)],
-          accuser.address
-        );
 
-      // verify accuser and embalmer have received the expected funds
-      expect(await getSarquitoBalance(accuser.address)).to.equal(
-        BigNumber.from(accusedArchaeologist.diggingFee).div(2).toString()
+      // snapshot all archaeologist free bond values prior to accusal
+      const archaeologistAddressesToInitialFreeBondsSarquitos: Map<
+        string,
+        BigNumber
+      > = new Map();
+      (
+        await viewStateFacet.getArchaeologistProfiles(
+          archaeologists.map((archaeologist) => archaeologist.archAddress)
+        )
+      ).forEach((archaeologistProfile, index) =>
+        archaeologistAddressesToInitialFreeBondsSarquitos.set(
+          archaeologists[index].archAddress,
+          archaeologistProfile.freeBond
+        )
       );
 
-      expect(await getSarquitoBalance(embalmer.address)).to.equal(
-        BigNumber.from(accusedArchaeologist.diggingFee)
-          .div(2)
-          .add(BigNumber.from(accusedArchaeologist.diggingFee))
-          .add(embalmerPreAccuseSarquitoBalance)
-          .toString()
-      );
-
-      // save the sarquito balance of the embalmer and the accuser after the first accusal
-      const embalmerPostAccuseSarquitoBalance = await getSarquitoBalance(
-        embalmer.address
-      );
-      const accuserPostAccuseSarquitoBalance = await getSarquitoBalance(
+      // accuse two archaeologists of leaking a keyshare
+      const tx1 = thirdPartyFacet.connect(accuser).accuse(
+        sarcophagus.sarcoId,
+        accusedArchaeologists
+          .slice(0, 2)
+          .map((accusedArchaeologist) => hashShare(accusedArchaeologist.share)),
         accuser.address
       );
 
-      // accuse the same archaeologist a second time
-      const tx = thirdPartyFacet
-        .connect(accuser)
-        .accuse(
-          sarcophagus.sarcoId,
-          [hashShare(accusedArchaeologist.share)],
-          accuser.address
+      // verify that AccuseArchaeologist is emitted
+      await expect(tx1).to.emit(thirdPartyFacet, `AccuseArchaeologist`);
+
+      // accuse one more archaeologist of leaking a keyshare
+      const tx2 = thirdPartyFacet.connect(accuser).accuse(
+        sarcophagus.sarcoId,
+        accusedArchaeologists
+          .slice(2, 3)
+          .map((accusedArchaeologist) => hashShare(accusedArchaeologist.share)),
+        accuser.address
+      );
+
+      // verify that AccuseArchaeologist is emitted
+      await expect(tx2).to.emit(thirdPartyFacet, `AccuseArchaeologist`);
+
+      const combinedDiggingFeesSarquito: BigNumber =
+        accusedArchaeologists.reduce(
+          (sum: BigNumber, accusedArchaeologist: ArchaeologistData) =>
+            sum.add(BigNumber.from(accusedArchaeologist.diggingFee)),
+          BigNumber.from(0)
         );
-
-      // verify that AccuseArchaeologist is not emitted
-      await expect(tx).not.to.emit(thirdPartyFacet, `AccuseArchaeologist`);
-
-      // verify that the accuser balance is unchanged
+      // verify that the accuser receives half of archaeologist cursed bond (equal to digging fee)
       expect(await getSarquitoBalance(accuser.address)).to.equal(
-        accuserPostAccuseSarquitoBalance.toString()
+        BigNumber.from(combinedDiggingFeesSarquito).div(2).toString()
       );
 
-      // verify that the embalmer balance is unchanged
-      expect(await getSarquitoBalance(embalmer.address)).to.equal(
-        embalmerPostAccuseSarquitoBalance.toString()
+      const embalmerPostAccuseSarquitoBalance = await getSarquitoBalance(
+        embalmer.address
+      );
+      // verify embalmer receives half of archaeologist cursed bond plus full digging fee
+      expect(
+        embalmerPostAccuseSarquitoBalance
+          .sub(embalmerPreAccuseSarquitoBalance)
+          .toString()
+      ).to.equal(
+        BigNumber.from(combinedDiggingFeesSarquito)
+          .div(2)
+          // todo: refund digging fees for freed archaeologists too?
+          .add(combinedDiggingFeesSarquito)
+          .toString()
       );
 
-      // verify accused archaeologist bond has been slashed
-      const accusedArchaeologistProfile =
-        await viewStateFacet.getArchaeologistProfile(
-          accusedArchaeologist.archAddress
+      const accusedArchaeologistProfiles =
+        await viewStateFacet.getArchaeologistProfiles(
+          accusedArchaeologists.map(
+            (accusedArchaeologist) => accusedArchaeologist.archAddress
+          )
         );
-      expect(accusedArchaeologistProfile.cursedBond.toString()).to.equal("0");
+      const innocentArchaeologistProfiles =
+        await viewStateFacet.getArchaeologistProfiles(
+          innocentArchaeologists.map(
+            (innocentArchaeologist) => innocentArchaeologist.archAddress
+          )
+        );
+      // verify accused archaeologists cursed bonds have zeroed and free bonds haven't been increased
+      accusedArchaeologistProfiles.forEach(
+        (accusedArchaeologistProfile, index) => {
+          expect(accusedArchaeologistProfile.cursedBond.toString()).to.equal(
+            "0"
+          );
+          // archaeologist's free bond prior to accusal
+          const initialFreeBond =
+            archaeologistAddressesToInitialFreeBondsSarquitos.get(
+              accusedArchaeologists[index].archAddress
+            );
+          expect(accusedArchaeologistProfile.freeBond.toString()).to.equal(
+            initialFreeBond
+          );
+        }
+      );
 
-      // verify accused archaeologist has been marked as accused
-      const accusedArchaeologistStorage =
-        await viewStateFacet.getSarcophagusArchaeologist(
-          sarcophagus.sarcoId,
-          accusedArchaeologist.archAddress
-        );
-      expect(accusedArchaeologistStorage.accused).to.be.true;
+      // verify innocent archaeologist bond has been freed - cursed bond set to zero and free bond increased by digging fee (cursed bond amount)
+      innocentArchaeologistProfiles.forEach(
+        (innocentArchaeologistProfile, index) => {
+          expect(innocentArchaeologistProfile.cursedBond.toString()).to.equal(
+            "0"
+          );
+          // archaeologist's free bond prior to accusal
+          const initialFreeBond =
+            archaeologistAddressesToInitialFreeBondsSarquitos.get(
+              innocentArchaeologists[index].archAddress
+            );
+          expect(innocentArchaeologistProfile.freeBond.toString()).to.equal(
+            initialFreeBond!.add(innocentArchaeologists[index].diggingFee)
+          );
+        }
+      );
 
-      // verify the sarcoId has only been added to the accused archaeologist's archaeologistAccusals once
-      const accusedArchaeologistAccusalsCount =
-        await viewStateFacet.getArchaeologistAccusalsCount(
-          accusedArchaeologist.archAddress
-        );
-      expect(accusedArchaeologistAccusalsCount.toString()).to.equal("1");
+      // verify accused archaeologists have been marked as accused
+      await verifyAccusalStatusesForArchaeologists(
+        sarcophagus.sarcoId,
+        accusedArchaeologists,
+        true
+      );
+
+      // verify innocent archaeologists have not been marked as accused
+      await verifyAccusalStatusesForArchaeologists(
+        sarcophagus.sarcoId,
+        innocentArchaeologists,
+        false
+      );
+
+      // verify the sarcoId has been added to the accused archaeologist's archaeologistAccusals
+      await Promise.all(
+        accusedArchaeologists.map(async (accusedArchaeologist) => {
+          const accusedArchaeologistAccusalsCount =
+            await viewStateFacet.getArchaeologistAccusalsCount(
+              accusedArchaeologist.archAddress
+            );
+          expect(accusedArchaeologistAccusalsCount.toString()).to.equal("1");
+        })
+      );
+
+      // verify the sarcoId has not been added to the innocent archaeologist's archaeologistAccusals
+      await Promise.all(
+        innocentArchaeologists.map(async (innocentArchaeologist) => {
+          const innocentArchaeologistAccusalsCount =
+            await viewStateFacet.getArchaeologistAccusalsCount(
+              innocentArchaeologist.archAddress
+            );
+          expect(innocentArchaeologistAccusalsCount.toString()).to.equal("0");
+        })
+      );
     });
-
-    it(
-      "Should allow accusal of 2 archaeologists on a 3 of 5 sarcophagus without freeing all other archaeologists"
-    );
-    it(
-      "Should free all unaccused archaeologists upon successful accusal of 3 archaeologists on a 3 of 5 sarcophagus and update state to accused"
-    );
-
-    it(
-      "Should free all unaccused archaeologists upon successful accusal of 1 archaeologist on a 3 of 5 sarcophagus where 2 have been accused on a previous call and update state to accused"
-    );
   });
 
   // todo: can an async context be used with mocha?
-  // context(
-  //   "On a successful accusal of a single archaeologist on a sarcophagus with no prior accusals",
-  //   async () => {
-  //     const { thirdPartyFacet, sarcoToken } = await getContracts();
-  //     const accuser = await getFreshAccount();
-  //
-  //     const { embalmer, sarcophagus, archaeologists } =
-  //       await generateSarcophagusWithArchaeologists({
-  //         totalShares: 5,
-  //         threshold: 3,
-  //         maximumRewrapIntervalSeconds: time.duration.weeks(4),
-  //         resurrectionTimeSeconds:
-  //           (await time.latest()) + time.duration.weeks(3),
-  //       });
-  //
-  //     const accusedArchaeologist = archaeologists[0];
-  //     // hash the leaked keyshare
-  //     const hashedShare = hashShare(accusedArchaeologist.share);
-  //
-  //     const embalmerPreAccuseSarquitoBalance = await sarcoToken.balanceOf(
-  //       embalmer.address
-  //     );
-  //
-  //     // accuse an archaeologist of leaking a keyshare
-  //     const tx = (await getContracts()).thirdPartyFacet
-  //       .connect(accuser)
-  //       .accuse(sarcophagus.sarcoId, [hashedShare], accuser.address);
-  //
-  //     // verify AccuseArchaeologist is emitted
-  //     it("Should emit AccuseArchaeologist", async () => {
-  //       await expect(tx).to.emit(thirdPartyFacet, `AccuseArchaeologist`);
-  //     });
-  //
-  //     // verify accuser receives half of archaeologist cursed bond (equal to digging fee)
-  //     it("Should transfer half of the archaeologist's cursed bond to the supplied payment address", async () => {
-  //       expect(await sarcoToken.balanceOf(accuser.address)).to.equal(
-  //         BigNumber.from(accusedArchaeologist.diggingFee).div(2).toString()
-  //       );
-  //     });
-  //
-  //     // verify embalmer receives half of archaeologist cursed bond plus full digging fee
-  //     it("Should transfer half of the archaeologist's cursed bond plus the digging fees to the embalmer", async () => {
-  //       const embalmerPostAccuseSarquitoBalance = await sarcoToken.balanceOf(
-  //         embalmer.address
-  //       );
-  //       expect(
-  //         embalmerPostAccuseSarquitoBalance
-  //           .sub(embalmerPreAccuseSarquitoBalance)
-  //           .toString()
-  //       ).to.equal(
-  //         BigNumber.from(accusedArchaeologist.diggingFee)
-  //           .div(2)
-  //           .add(BigNumber.from(accusedArchaeologist.diggingFee))
-  //           .toString()
-  //       );
-  //     });
-  //
-  //     // verify accused archaeologist bond has been slashed
-  //     // verify accused archaeologist has been marked as accused
-  //     // verify the accused archaeologist has been added to archaeologistAccusals
-  //   }
-  // );
+  context(
+    "On a successful accusal of a single archaeologist on a sarcophagus with no prior accusals",
+    async () => {
+      const { thirdPartyFacet, sarcoToken } = await getContracts();
+      const accuser = await getFreshAccount();
+
+      const { embalmer, sarcophagus, archaeologists } =
+        await generateSarcophagusWithArchaeologists({
+          totalShares: 5,
+          threshold: 3,
+          maximumRewrapIntervalSeconds: time.duration.weeks(4),
+          resurrectionTimeSeconds:
+            (await time.latest()) + time.duration.weeks(3),
+        });
+
+      const accusedArchaeologist = archaeologists[0];
+      // hash the leaked keyshare
+      const hashedShare = hashShare(accusedArchaeologist.share);
+
+      const embalmerPreAccuseSarquitoBalance = await sarcoToken.balanceOf(
+        embalmer.address
+      );
+
+      // accuse an archaeologist of leaking a keyshare
+      const tx = (await getContracts()).thirdPartyFacet
+        .connect(accuser)
+        .accuse(sarcophagus.sarcoId, [hashedShare], accuser.address);
+
+      // verify AccuseArchaeologist is emitted
+      it("Should emit AccuseArchaeologist", async () => {
+        await expect(tx).to.emit(thirdPartyFacet, `AccuseArchaeologist`);
+      });
+
+      // verify accuser receives half of archaeologist cursed bond (equal to digging fee)
+      it("Should transfer half of the archaeologist's cursed bond to the supplied payment address", async () => {
+        expect(await sarcoToken.balanceOf(accuser.address)).to.equal(
+          BigNumber.from(accusedArchaeologist.diggingFee).div(2).toString()
+        );
+      });
+
+      // verify embalmer receives half of archaeologist cursed bond plus full digging fee
+      it("Should transfer half of the archaeologist's cursed bond plus the digging fees to the embalmer", async () => {
+        const embalmerPostAccuseSarquitoBalance = await sarcoToken.balanceOf(
+          embalmer.address
+        );
+        expect(
+          embalmerPostAccuseSarquitoBalance
+            .sub(embalmerPreAccuseSarquitoBalance)
+            .toString()
+        ).to.equal(
+          BigNumber.from(accusedArchaeologist.diggingFee)
+            .div(2)
+            .add(BigNumber.from(accusedArchaeologist.diggingFee))
+            .toString()
+        );
+      });
+    }
+  );
 });
