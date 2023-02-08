9f35a1daba92cf0f5f2bc0be06d9b150c6f44857
knoll3 <kyle.noll@decentlabs.io>
2023-02-08 15:31:35 -0500

    Validate new res time is less than max res time

--- contracts/facets/EmbalmerFacet.sol
+++ contracts/facets/EmbalmerFacet.sol
@@ -136,6 +136,15 @@ contract EmbalmerFacet {
         uint256 maximumPermissibleResurrectionTime
     );
 
+    /// @notice Emitted when the new resurrection time during rewrap exceeds the maximum allowed
+    /// resurrection time
+    /// @param resurrectionTime The new resurrection time submitted during rewrap
+    /// @param maxResurrectionTime The maximum allowed resurrection time stored on the sarcophagus
+    error NewResurrectionTimeExceedsMaxResurrectionTime(
+        uint256 resurrectionTime,
+        uint256 maxResurrectionTime
+    );
+
     /// @notice Creates a sarcophagus with the supplied parameters and locks
     /// a portion of each archaeologist's freeBond equal to the diggingFees for the sarcophagus.
     /// Verifies that all supplied archaeologists have signed off on the sarcophagus negotiation parameters:
@@ -351,6 +360,14 @@ contract EmbalmerFacet {
                 resurrectionTime,
                 sarcophagus.maximumRewrapInterval,
                 block.timestamp + sarcophagus.maximumRewrapInterval
+            );
+        }
+
+        // Confirm that the new resurrection time doesn't exceed the maximumResurrectionTime
+        if (sarcophagus.maximumResurrectionTime < resurrectionTime) {
+            revert NewResurrectionTimeExceedsMaxResurrectionTime(
+                resurrectionTime,
+                sarcophagus.maximumResurrectionTime
             );
         }
 
