0fd34da0c6335169547f85ef7ca3d475308ba0a4
Kellar <kellar.base09@gmail.com>
2023-03-27 19:10:30 +0100

    Add arch address to  event

--- contracts/facets/ArchaeologistFacet.sol
+++ contracts/facets/ArchaeologistFacet.sol
@@ -13,7 +13,11 @@ contract ArchaeologistFacet {
     /// @notice Emitted when an archaeologist successfully publishes their private key for a sarcophagus
     /// @param sarcoId ID of sarcophagus archaeologist has published the private key on
     /// @param privateKey private key that has been published
-    event PublishPrivateKey(bytes32 indexed sarcoId, bytes32 privateKey);
+    event PublishPrivateKey(
+        bytes32 indexed sarcoId,
+        bytes32 privateKey,
+        address indexed archaeologist
+    );
 
     event DepositFreeBond(address indexed archaeologist, uint256 depositedBond);
 
@@ -281,6 +285,6 @@ contract ArchaeologistFacet {
         // Save the successful sarcophagus against the archaeologist
         s.archaeologistSuccesses[msg.sender].push(sarcoId);
 
-        emit PublishPrivateKey(sarcoId, privateKey);
+        emit PublishPrivateKey(sarcoId, privateKey, msg.sender);
     }
 }
