9df84154f8edfb753fbf929826591e76bd6e669b
sethhrbek <seth.hrbek@gmail.com>
2023-04-06 08:43:56 -0400

    run prettier

--- test/facets/helpers/sarcophagus.ts
+++ test/facets/helpers/sarcophagus.ts
@@ -117,7 +117,7 @@ export const createSarcophagusData = async (params: {
     creationTimeSeconds,
     privateKeys,
     publicKeys,
-    cursedBondPercentage
+    cursedBondPercentage,
   };
 };
 
