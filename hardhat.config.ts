f56c949057ca41b6397e8d5b8015ed097af78444
Seth Hrbek <seth.hrbek@gmail.com>
2023-06-29 10:55:20 -0400

    update version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,21 +73,21 @@ const config: HardhatUserConfig = {
   networks: {
     mainnet: {
       chainId: 1,
-      url: process.env.MAINNET_PROVIDER,
+      url: process.env.MAINNET_PROVIDER || "",
       accounts: process.env.MAINNET_DEPLOYER_PRIVATE_KEY
         ? [process.env.MAINNET_DEPLOYER_PRIVATE_KEY]
         : [],
     },
     goerli: {
       chainId: 5,
-      url: process.env.GOERLI_PROVIDER,
+      url: process.env.GOERLI_PROVIDER || "",
       accounts: process.env.GOERLI_DEPLOYER_PRIVATE_KEY
         ? [process.env.GOERLI_DEPLOYER_PRIVATE_KEY]
         : [],
     },
     sepolia: {
       chainId: 11155111,
-      url: process.env.SEPOLIA_PROVIDER,
+      url: process.env.SEPOLIA_PROVIDER || "",
       accounts: process.env.SEPOLIA_DEPLOYER_PRIVATE_KEY
         ? [process.env.SEPOLIA_DEPLOYER_PRIVATE_KEY]
         : [],
